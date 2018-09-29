6ff4c6ee608074095ffe92db329219805d2af7e9
Daniel Fischer <d.fischer874@gmail.com>
2018-09-29 16:09:55 -0400

    Showing the cell's value only after clicking it

--- src/Cell.js
+++ src/Cell.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import './Cell.css';
 
 class Cell extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            shown: false
+        };
+    }
+
     render() {
-	return <span>&nbsp;{this.props.value}&nbsp;</span>;
+        const content = this.state.shown ? this.props.value : '';
+	return <span className="cell" onClick={() => this.handleClick()}>&nbsp;{content}&nbsp;</span>;
+    }
+
+    handleClick() {
+        this.setState({
+            shown: true
+        });
     }
     
 }
