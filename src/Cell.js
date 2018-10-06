8aab778b4b72d8ee78e9e594a0c90eca4d2441f7
Daniel Fischer <d.fischer874@gmail.com>
2018-10-05 20:11:56 -0400

    Added color to revealed numbers

--- src/Cell.js
+++ src/Cell.js
@@ -5,7 +5,8 @@ class Cell extends Component {
 
     render() {
 	const contentToUnveil = this.getContentToUnveil(this.props.value, this.props.status, this.props.surroundingMineCountFn);
-	return <td className="cell" onClick={(e) => this.props.handleClickFn(e)}><span>&nbsp;{contentToUnveil}&nbsp;</span></td>;
+	const classes = this.getClasses(this.props.value, this.props.status, this.props.surroundingMineCountFn);
+	return <td className={classes} onClick={(e) => this.props.handleClickFn(e)}><span>&nbsp;{contentToUnveil}&nbsp;</span></td>;
     }
 
     getContentToUnveil(cellValue, cellStatus, surroundingMineCountFn) {
@@ -25,6 +26,37 @@ class Cell extends Component {
 	    contentToUnveil = '';
 	}
 	return contentToUnveil;
+    }
+
+    getClasses(cellValue, cellStatus, surroundingMineCountFn) {
+	let classes;
+	if( cellStatus === 'REVEALED' && cellValue !== 'MINE' ) {
+	    let mineCountClass;
+	    const surroundingMineCount = surroundingMineCountFn();
+	    if( surroundingMineCount === 1 ) {
+		mineCountClass = "one";
+	    } else if (surroundingMineCount === 2) {
+		mineCountClass = "two";
+	    } else if (surroundingMineCount === 3) {
+		mineCountClass = "three";
+	    } else if (surroundingMineCount === 4) {
+		mineCountClass = "four";
+	    } else if (surroundingMineCount === 5) {
+		mineCountClass = "five";
+	    } else if (surroundingMineCount === 6) {
+		mineCountClass = "six";
+	    } else if (surroundingMineCount === 7) {
+		mineCountClass = "seven";
+	    } else if (surroundingMineCount === 8) {
+		mineCountClass = "eight";
+	    }
+	    classes = `cell ${mineCountClass}`;
+	} else if (cellStatus === 'REVEALED' && cellValue === 'MINE') {
+	    classes = 'cell mine';
+	} else {
+	    classes = 'cell';
+	}
+	return classes;
     }
 
 }
--- src/Cell.test.js
+++ src/Cell.test.js
@@ -14,3 +14,19 @@ test('getContentToUnveil returns a M is value is MINE', () => {
     expect(new Cell().getContentToUnveil('', 'HIDDEN', () => 1)).toBe('');
     expect(new Cell().getContentToUnveil('MINE', 'HIDDEN', () => 1)).toBe('');
 });
+
+test('getClasses', () => {
+    expect(new Cell().getClasses('', 'REVEALED', () => 1)).toBe('cell one');
+    expect(new Cell().getClasses('', 'REVEALED', () => 2)).toBe('cell two');
+    expect(new Cell().getClasses('', 'REVEALED', () => 3)).toBe('cell three');
+    expect(new Cell().getClasses('', 'REVEALED', () => 4)).toBe('cell four');
+    expect(new Cell().getClasses('', 'REVEALED', () => 5)).toBe('cell five');
+    expect(new Cell().getClasses('', 'REVEALED', () => 6)).toBe('cell six');
+    expect(new Cell().getClasses('', 'REVEALED', () => 7)).toBe('cell seven');
+    expect(new Cell().getClasses('', 'REVEALED', () => 8)).toBe('cell eight');
+    expect(new Cell().getClasses('', 'HIDDEN', () => 2)).toBe('cell');
+    expect(new Cell().getClasses('', 'HIDDEN', () => 4)).toBe('cell');
+    expect(new Cell().getClasses('', 'HIDDEN', () => 7)).toBe('cell');
+    expect(new Cell().getClasses('MINE', 'HIDDEN', () => 4)).toBe('cell');
+    expect(new Cell().getClasses('MINE', 'REVEALED', () => 7)).toBe('cell mine');
+});
